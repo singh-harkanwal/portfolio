0d02f3f634bee5175829e433655caf2a2a933979
Elegant HTML <68264793+eleganthtml@users.noreply.github.com>
2020-11-16 00:30:29 +1030

    Create action.php

---
+++ action.php
@@ -0,0 +1,17 @@
+      <?PHP
+      if(isset($_POST['submit']) && $_POST['phone'] == ''){
+        $to = "Kanwal.engineer@gmail.com";
+        $from = $_POST['Email'];
+        $name = $_POST['Name'];
+        $subject = "Form submission";
+        $message = $name . " wrote the following:" . "\n\n" . $_POST['Message'] . "From" . $from;
+
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // More headers
+        mail($to,$subject,$message,$headers);
+        
+        echo "The email has been sent, I will contact you soon."
+      }
+      ?>
